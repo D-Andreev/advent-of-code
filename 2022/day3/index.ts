f45d5d52c5028560bbda7f4be85c5f217fc20228
Dimitar Andreev <d.andreev90@gmail.com>
2022-12-04 09:08:32 +0200

    day 4

--- 2022/day3/index.ts
+++ 2022/day3/index.ts
@@ -1,82 +1,84 @@
 const path = require('path');
 const {readInput} = require('../../helpers');
 
-const UPPERCASE_SUBTRACT_COUNT = 38;
-const LOWERCASE_SUBTRACT_COUNT = 96;
-type Compartment = string[];
-type ItemsMap = {[k: string]: number};
+namespace day3 {
+  const UPPERCASE_SUBTRACT_COUNT = 38;
+  const LOWERCASE_SUBTRACT_COUNT = 96;
+  type Compartment = string[];
+  type ItemsMap = {[k: string]: number};
 
-const input = readInput(path.join(__dirname, 'input.prod.txt'));
+  const input = readInput(path.join(__dirname, 'input.prod.txt'));
 
-function splitRucksackIntoCompartments(rucksack: string): Compartment[] {
-  const rucksackArr = rucksack.split('');
-  return [
-    rucksackArr.slice(0, rucksackArr.length / 2),
-    rucksackArr.slice(rucksackArr.length / 2, rucksackArr.length)
-  ];
-}
-
-function countItemsInRucksack(compartment: Compartment, itemsMap: ItemsMap) {
-  compartment.forEach((item) => {
-    if (!itemsMap.hasOwnProperty(item)) {
-      itemsMap[item] = 0;
-    }
-    itemsMap[item]++;
-  });
-}
-
-function getItemPriority(item: string): number {
-  return item === item.toUpperCase() ?
-    item.charCodeAt(0) - UPPERCASE_SUBTRACT_COUNT: item.charCodeAt(0) - LOWERCASE_SUBTRACT_COUNT;
-}
-
-function getSumOfPriorities(repeatingItems: string[]): number {
-  return repeatingItems
-    .reduce((partialSum, item) => partialSum + getItemPriority(item), 0);
-}
-
-function partOne() {
-  const repeatingItems = [];
-  for (let i = 0; i < input.length; i++) {
-    const rucksack = input[i];
-    const compartments = splitRucksackIntoCompartments(rucksack);
-    const items = {};
-    countItemsInRucksack(compartments[0], items);
-    for (let j = 0; j < compartments[1].length; j++) {
-      const item = compartments[1][j];
-      if (items.hasOwnProperty(item)) {
-        repeatingItems.push(item);
-        break;
-      }
-    }
+  function splitRucksackIntoCompartments(rucksack: string): Compartment[] {
+    const rucksackArr = rucksack.split('');
+    return [
+      rucksackArr.slice(0, rucksackArr.length / 2),
+      rucksackArr.slice(rucksackArr.length / 2, rucksackArr.length)
+    ];
   }
 
-  return getSumOfPriorities(repeatingItems);
-}
-
-function partTwo() {
-  const repeatingItems = [];
-  for (let i = 0; i < input.length; i+=3) {
-    const rucksack1 = input[i].split('');
-    const rucksack2 = input[i + 1].split('');
-    const rucksack3 = input[i + 2].split('');
-
-    const firstRucksackItems = {};
-    const secondRucksackItems = {};
-    countItemsInRucksack(rucksack1, firstRucksackItems);
-    countItemsInRucksack(rucksack2, secondRucksackItems);
-
-    for (let j = 0; j < rucksack3.length; j++) {
-      const item = rucksack3[j];
-      if (firstRucksackItems.hasOwnProperty(item) && secondRucksackItems.hasOwnProperty(item)) {
-        repeatingItems.push(item);
-        break;
+  function countItemsInRucksack(compartment: Compartment, itemsMap: ItemsMap) {
+    compartment.forEach((item) => {
+      if (!itemsMap.hasOwnProperty(item)) {
+        itemsMap[item] = 0;
       }
-    }
+      itemsMap[item]++;
+    });
   }
 
-  return getSumOfPriorities(repeatingItems);
-}
+  function getItemPriority(item: string): number {
+    return item === item.toUpperCase() ?
+      item.charCodeAt(0) - UPPERCASE_SUBTRACT_COUNT: item.charCodeAt(0) - LOWERCASE_SUBTRACT_COUNT;
+  }
 
-console.log(partOne());
-console.log(partTwo());
+  function getSumOfPriorities(repeatingItems: string[]): number {
+    return repeatingItems
+      .reduce((partialSum, item) => partialSum + getItemPriority(item), 0);
+  }
+
+  function partOne() {
+    const repeatingItems = [];
+    for (let i = 0; i < input.length; i++) {
+      const rucksack = input[i];
+      const compartments = splitRucksackIntoCompartments(rucksack);
+      const items = {};
+      countItemsInRucksack(compartments[0], items);
+      for (let j = 0; j < compartments[1].length; j++) {
+        const item = compartments[1][j];
+        if (items.hasOwnProperty(item)) {
+          repeatingItems.push(item);
+          break;
+        }
+      }
+    }
+
+    return getSumOfPriorities(repeatingItems);
+  }
+
+  function partTwo() {
+    const repeatingItems = [];
+    for (let i = 0; i < input.length; i+=3) {
+      const rucksack1 = input[i].split('');
+      const rucksack2 = input[i + 1].split('');
+      const rucksack3 = input[i + 2].split('');
+
+      const firstRucksackItems = {};
+      const secondRucksackItems = {};
+      countItemsInRucksack(rucksack1, firstRucksackItems);
+      countItemsInRucksack(rucksack2, secondRucksackItems);
+
+      for (let j = 0; j < rucksack3.length; j++) {
+        const item = rucksack3[j];
+        if (firstRucksackItems.hasOwnProperty(item) && secondRucksackItems.hasOwnProperty(item)) {
+          repeatingItems.push(item);
+          break;
+        }
+      }
+    }
+
+    return getSumOfPriorities(repeatingItems);
+  }
+
+  console.log(partOne());
+  console.log(partTwo());
+}
---
+++ 2022/day4/index.ts
@@ -0,0 +1,67 @@
+import {readInput} from '../../helpers';
+const path = require('path');
+
+namespace day4 {
+  const {readInput} = require('../../helpers');
+
+  const input = readInput(path.join(__dirname, 'input.prod.txt'));
+
+  class Section {
+    public start: number;
+    public end: number;
+
+    constructor(data: string[]) {
+      this.start = parseInt(data[0], 10);
+      this.end = parseInt(data[1], 10);
+    }
+  }
+
+  type Pair = {
+    firstElfSection: Section;
+    secondElfSection: Section;
+  }
+
+  function parsePair(inputRow: string): Pair {
+    const pairs = inputRow.split(',');
+    const firstElfSection = pairs[0].split('-');
+    const secondElfSection = pairs[1].split('-');
+
+    return {
+      firstElfSection: new Section(firstElfSection),
+      secondElfSection: new Section(secondElfSection),
+    };
+  }
+
+  function partOne() {
+    return input.reduce((fullyOverlappingPairs: number, inputRow: string) => {
+      const {firstElfSection, secondElfSection} = parsePair(inputRow);
+      if (firstElfSection.start <= secondElfSection.start &&
+        firstElfSection.end >= secondElfSection.end) {
+        fullyOverlappingPairs++;
+      } else if (secondElfSection.start <= firstElfSection.start &&
+        secondElfSection.end >= firstElfSection.end) {
+        fullyOverlappingPairs++;
+      }
+
+      return fullyOverlappingPairs;
+    }, 0);
+  }
+
+  function partTwo() {
+    return input.reduce((overlappingPairs: number, inputRow: string) => {
+      const {firstElfSection, secondElfSection} = parsePair(inputRow);
+      if (secondElfSection.start >= firstElfSection.start &&
+        secondElfSection.start <= firstElfSection.end) {
+        overlappingPairs++;
+      } else if (firstElfSection.start >= secondElfSection.start &&
+                firstElfSection.start <= secondElfSection.end) {
+        overlappingPairs++;
+      }
+
+      return overlappingPairs;
+    }, 0);
+  }
+
+  console.log(partOne());
+  console.log(partTwo());
+}
